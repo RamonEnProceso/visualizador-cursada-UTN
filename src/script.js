7fe31bbecc825c6669eccc4fa71a73154d1b6770
Ramón <ramon_en_proceso@pop-os.fibertel.com.ar>
2025-10-03 08:33:47 -0300

    feat: filtros dinámicos (desactivar botones, mostrar cursados, oscurecer no aprobados)

--- src/script.js
+++ src/script.js
@@ -10,6 +10,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const buttonContainer = document.getElementById("button_careers_container");
 const tableContainer = document.getElementById("table_of_contents_container");
+const inputButtonOff = document.getElementById("buttons-off");
+const inputOnlyCoursed = document.getElementById("only-coursed");
+const inputDarkNotApproved = document.getElementById("dark-not-approved");
 const careersData = {};
 const approvedSubjects = {};
 const coursedSubjects = {};
@@ -49,23 +52,92 @@ const renderCareer = (careerId) => {
         const levelHtml = document.createElement("div");
         const headerHtml = document.createElement("div");
         headerHtml.textContent = levelName;
+        headerHtml.className = "content_header";
         levelHtml.appendChild(headerHtml);
         numLevels++;
+        levelHtml.className = "content_column";
         //Acá se crea "el header" de la columna
-        let electivasNumber = 0;
+        let electiveNumber = 0;
         level.subjects.forEach((subject) => {
             //Acá se crean las distintas materias
             const div = document.createElement("div");
-            div.textContent = subject.name;
+            const divSpan = document.createElement("span");
+            divSpan.textContent = subject.name;
+            divSpan.className = "content_name";
+            div.appendChild(divSpan);
+            div.className = "content_subjects none";
+            const updateDivStatus = (cb) => {
+                if (!div)
+                    return;
+                if (cb.id.includes("coursed")) {
+                    cb.checked ? div.classList.add("coursed") : div.classList.remove("coursed");
+                    div.classList.remove("none");
+                }
+                if (cb.id.includes("approved")) {
+                    cb.checked ? div.classList.add("approved") : div.classList.remove("approved");
+                    div.classList.remove("none");
+                }
+                if (!div.classList.contains("coursed") && !div.classList.contains("approved")) {
+                    div.classList.add("none");
+                }
+            };
+            const createCheckboxes = (dv, id) => {
+                const approvedCheck = document.createElement("input");
+                approvedCheck.type = "checkbox";
+                approvedCheck.id = `${id}-approved`;
+                approvedCheck.className = "checkbox";
+                approvedCheck.addEventListener("change", () => {
+                    if (approvedCheck.checked) {
+                        coursedCheck.checked = true;
+                        coursedSubjects[id] = true;
+                        updateDivStatus(coursedCheck);
+                    }
+                    approvedSubjects[id] = approvedCheck.checked;
+                    updateDivStatus(approvedCheck);
+                    darkNotApproved();
+                });
+                const approvedLabel = document.createElement("label");
+                approvedLabel.textContent = "Aprobado";
+                approvedLabel.className = "checkboxLabel";
+                approvedLabel.htmlFor = `${id}-approved`;
+                const coursedCheck = document.createElement("input");
+                coursedCheck.type = "checkbox";
+                coursedCheck.id = `${id}-coursed`;
+                coursedCheck.className = "checkbox";
+                const coursedLabel = document.createElement("label");
+                coursedLabel.textContent = "Cursado";
+                coursedLabel.className = "checkboxLabel";
+                coursedLabel.htmlFor = `${id}-coursed`;
+                coursedSubjects[id] = coursedCheck.checked;
+                coursedCheck.addEventListener("change", () => {
+                    if (!coursedCheck.checked) {
+                        approvedCheck.checked = false;
+                        approvedSubjects[id] = false;
+                        updateDivStatus(approvedCheck);
+                    }
+                    coursedSubjects[id] = coursedCheck.checked;
+                    updateDivStatus(coursedCheck);
+                    darkNotApproved();
+                });
+                dv.appendChild(coursedCheck);
+                dv.appendChild(coursedLabel);
+                dv.appendChild(approvedCheck);
+                dv.appendChild(approvedLabel);
+            };
             if (subject.elective) {
                 //Acá debo poner para elegir la cantidad de electivas
                 const number = subject.number || 1;
                 for (let i = 0; number > i; i++) {
+                    const divElective = document.createElement("div");
+                    divElective.className = "elective_div";
                     const clonedSelect = electivesSelect.cloneNode(true);
-                    clonedSelect.id = `elective-select-${electivasNumber}`;
-                    electivasNumber++;
-                    div.appendChild(clonedSelect);
+                    clonedSelect.id = `elective-select-${electiveNumber}`;
+                    clonedSelect.className = "subject_select";
+                    electiveNumber++;
+                    divElective.appendChild(clonedSelect);
                     clonedSelect.addEventListener("change", () => {
+                        divElective.innerHTML = "";
+                        divElective.appendChild(clonedSelect);
                         const chosen = clonedSelect.value;
                         console.log("Elegiste:", chosen);
                         if (!chosenElectives[subject.id]) {
@@ -73,27 +145,24 @@ const renderCareer = (careerId) => {
                         }
                         chosenElectives[subject.id][i] = chosen;
                         console.log(chosenElectives);
+                        divElective.querySelectorAll("input").forEach(input => input.remove());
+                        const electiveOptionName = Array.from(clonedSelect.options).find(opt => opt.value === chosen);
+                        const electiveName = document.createElement("span");
+                        electiveName.className = "elective_name";
+                        electiveName.textContent = (electiveOptionName === null || electiveOptionName === void 0 ? void 0 : electiveOptionName.textContent) || "";
+                        divSpan.style.display = "none";
+                        divElective.appendChild(electiveName);
+                        createCheckboxes(divElective, chosen);
                     });
+                    div.appendChild(divElective);
                 }
             }
             levelHtml.appendChild(div);
-            const approvedCheck = document.createElement("input");
-            approvedCheck.type = "checkbox";
-            approvedCheck.id = subject.id;
-            approvedCheck.addEventListener("change", () => {
-                approvedSubjects[subject.id] = approvedCheck.checked;
-                localStorage.setItem("approvedSubjects", JSON.stringify(approvedSubjects));
-            });
-            div.appendChild(approvedCheck);
-            const coursedCheck = document.createElement("input");
-            coursedCheck.type = "checkbox";
-            coursedCheck.id = subject.id;
-            coursedSubjects[subject.id] = coursedCheck.checked;
-            approvedCheck.addEventListener("change", () => {
-                coursedSubjects[subject.id] = coursedCheck.checked;
-            });
-            div.appendChild(coursedCheck);
+            if (!subject.elective) {
+                createCheckboxes(div, subject.id);
+            }
         });
+        tableContainer.style.gridTemplateColumns = `repeat(${numLevels}, 1fr)`;
         tableContainer.appendChild(levelHtml);
     });
 };
@@ -106,4 +175,55 @@ const renderButtons = () => {
         buttonContainer.appendChild(li);
     });
 };
+const buttonOff = () => {
+    const inputCheckboxes = document.querySelectorAll(".checkboxLabel");
+    const selectSubject = document.querySelectorAll(".subject_select");
+    inputCheckboxes.forEach(cb => {
+        if (cb.style.display === "none") {
+            cb.style.display = "inline-block";
+        }
+        else {
+            cb.style.display = "none";
+        }
+    });
+    selectSubject.forEach(sl => {
+        if (sl.style.display === "none") {
+            sl.style.display = "inline-block";
+        }
+        else {
+            sl.style.display = "none";
+        }
+    });
+};
+const hideOnlyCoursed = () => {
+    const coursed = document.querySelectorAll("div.content_subjects:not(.coursed)");
+    coursed.forEach(div => {
+        if (div.style.display === "none") {
+            div.style.display = "inline-block";
+        }
+        else {
+            div.style.display = "none";
+        }
+    });
+};
+const darkNotApproved = () => {
+    const allDivs = document.querySelectorAll("div.content_subjects");
+    allDivs.forEach(div => {
+        if (div.classList.contains("approved")) {
+            div.style.opacity = "1";
+        }
+        else {
+            div.style.opacity = inputDarkNotApproved.checked ? "0.5" : "1";
+        }
+    });
+};
+inputButtonOff.addEventListener("change", () => {
+    buttonOff();
+});
+inputOnlyCoursed.addEventListener("change", () => {
+    hideOnlyCoursed();
+});
+inputDarkNotApproved.addEventListener("change", () => {
+    darkNotApproved();
+});
 loadCareer("sistemas", "../data/ingenieria_sistemas.json");
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,8 @@
 const buttonContainer : HTMLElement = document.getElementById("button_careers_container")!;
 const tableContainer : HTMLElement = document.getElementById("table_of_contents_container")!;
+const inputButtonOff : HTMLInputElement = document.getElementById("buttons-off") as HTMLInputElement;
+const inputOnlyCoursed : HTMLInputElement = document.getElementById("only-coursed") as HTMLInputElement;
+const inputDarkNotApproved : HTMLInputElement = document.getElementById("dark-not-approved") as HTMLInputElement;
 
 const careersData: Record<string, any> = {};
 const approvedSubjects: Record<string, boolean> = {};
@@ -71,30 +74,104 @@ const renderCareer = (careerId : string) =>{
         const levelHtml = document.createElement("div");
         const headerHtml = document.createElement("div");
         headerHtml.textContent = levelName;
+        headerHtml.className = "content_header"
         levelHtml.appendChild(headerHtml);
         numLevels++;
+        levelHtml.className = "content_column";
 
         //Acá se crea "el header" de la columna
 
-        let electivasNumber = 0;
+        let electiveNumber = 0;
 
         level.subjects.forEach((subject : Subject)=>{
             //Acá se crean las distintas materias
 
             const div = document.createElement("div");
-            div.textContent = subject.name
+            const divSpan = document.createElement("span");
+            divSpan.textContent = subject.name;
+            divSpan.className = "content_name"
+            div.appendChild(divSpan);
+            div.className = "content_subjects none";
+
+            const updateDivStatus = (cb: HTMLInputElement)=>{
+                if(!div) return;
+                if(cb.id.includes("coursed")){
+                    cb.checked ? div.classList.add("coursed"): div.classList.remove("coursed");
+                    div.classList.remove("none");
+                }
+                if(cb.id.includes("approved")){
+                    cb.checked ? div.classList.add("approved"): div.classList.remove("approved");
+                    div.classList.remove("none");
+                }
+                if (!div.classList.contains("coursed") && !div.classList.contains("approved")) {
+                    div.classList.add("none");
+                }
+            }
+
+            const createCheckboxes = (dv: HTMLElement, id : string) =>{
+                const approvedCheck = document.createElement("input");
+                approvedCheck.type="checkbox";
+                approvedCheck.id= `${id}-approved`;
+                approvedCheck.className = "checkbox";
+                approvedCheck.addEventListener("change",()=>{
+                    if(approvedCheck.checked){
+                        coursedCheck.checked = true;
+                        coursedSubjects[id] = true;
+                        updateDivStatus(coursedCheck)}
+                        approvedSubjects[id] = approvedCheck.checked;
+                        updateDivStatus(approvedCheck);
+                        darkNotApproved();
+                    })
+                const approvedLabel = document.createElement("label");
+                approvedLabel.textContent = "Aprobado"
+                approvedLabel.className = "checkboxLabel";
+                approvedLabel.htmlFor = `${id}-approved`;
+                        
+
+                const coursedCheck = document.createElement("input");
+                coursedCheck.type="checkbox";
+                coursedCheck.id= `${id}-coursed`;
+                coursedCheck.className = "checkbox";
+                const coursedLabel = document.createElement("label");
+                coursedLabel.textContent = "Cursado"
+                coursedLabel.className = "checkboxLabel";
+                coursedLabel.htmlFor = `${id}-coursed`
+
+                coursedSubjects[id] = coursedCheck.checked;
+                coursedCheck.addEventListener("change",()=>{
+                if(!coursedCheck.checked){
+                    approvedCheck.checked = false;
+                    approvedSubjects[id] = false;
+                    updateDivStatus(approvedCheck);}
+                    coursedSubjects[id] = coursedCheck.checked;
+                    updateDivStatus(coursedCheck);
+                    darkNotApproved();
+                })
+
+                dv.appendChild(coursedCheck);
+                dv.appendChild(coursedLabel);
+                dv.appendChild(approvedCheck);
+                dv.appendChild(approvedLabel)
+
+            }
 
             if(subject.elective){
                 //Acá debo poner para elegir la cantidad de electivas
                 const number = subject.number || 1;
 
                 for (let i = 0; number > i; i++){
+                    const divElective = document.createElement("div");
+                    divElective.className="elective_div";
                     const clonedSelect = electivesSelect.cloneNode(true) as HTMLSelectElement;
-                    clonedSelect.id = `elective-select-${electivasNumber}`
-                    electivasNumber++;
-                    div.appendChild(clonedSelect);
+                    clonedSelect.id = `elective-select-${electiveNumber}`
+                    clonedSelect.className = "subject_select"
+                    electiveNumber++;
+                    divElective.appendChild(clonedSelect);
 
                     clonedSelect.addEventListener("change",()=>{
+                        divElective.innerHTML="";
+                        divElective.appendChild(clonedSelect);
+                        
                         const chosen = clonedSelect.value; 
                         console.log("Elegiste:", chosen);
 
@@ -104,9 +181,22 @@ const renderCareer = (careerId : string) =>{
 
                         chosenElectives[subject.id][i] = chosen;
                         console.log(chosenElectives);
-                        
-                    })
 
+                        divElective.querySelectorAll("input").forEach(input => input.remove());
+
+                        const electiveOptionName = Array.from(clonedSelect.options).find(opt => opt.value === chosen);
+
+                        const electiveName = document.createElement("span");
+                        electiveName.className = "elective_name"
+                        electiveName.textContent = electiveOptionName?.textContent || "";
+
+                        divSpan.style.display = "none"
+
+                        divElective.appendChild(electiveName);
+                        createCheckboxes(divElective,chosen);
+
+                    })
+                    div.appendChild(divElective)
                 }
 
                 
@@ -114,26 +204,12 @@ const renderCareer = (careerId : string) =>{
 
             levelHtml.appendChild(div);
 
-            const approvedCheck = document.createElement("input");
-            approvedCheck.type="checkbox";
-            approvedCheck.id= subject.id;
-            approvedCheck.addEventListener("change",()=>{
-                approvedSubjects[subject.id] = approvedCheck.checked;
-                localStorage.setItem("approvedSubjects", JSON.stringify(approvedSubjects));
-            })
-
-            div.appendChild(approvedCheck)
-
-            const coursedCheck = document.createElement("input");
-            coursedCheck.type="checkbox";
-            coursedCheck.id= subject.id;
-            coursedSubjects[subject.id] = coursedCheck.checked;
-            approvedCheck.addEventListener("change",()=>{
-                coursedSubjects[subject.id] = coursedCheck.checked;
-            })
-            div.appendChild(coursedCheck)
+            if (!subject.elective){
+                createCheckboxes(div,subject.id);
+            }
 
         })
+        tableContainer.style.gridTemplateColumns = `repeat(${numLevels}, 1fr)`
         tableContainer.appendChild(levelHtml);
     })
 }
@@ -149,5 +225,58 @@ const renderButtons = () =>{
     })
 
 }
+
+const buttonOff = () =>{
+    const inputCheckboxes = document.querySelectorAll<HTMLElement>(".checkboxLabel");
+    const selectSubject = document.querySelectorAll<HTMLElement>(".subject_select");
+    inputCheckboxes.forEach(cb=>{
+        if (cb.style.display === "none"){
+            cb.style.display = "inline-block"
+        } else{
+            cb.style.display = "none"
+        }
+    })
+    selectSubject.forEach(sl=>{
+        if (sl.style.display === "none"){
+            sl.style.display = "inline-block"
+        } else{
+            sl.style.display = "none"
+        }
+    })
+}
+
+const hideOnlyCoursed = () =>{
+    const coursed = document.querySelectorAll<HTMLElement>("div.content_subjects:not(.coursed)");
+    coursed.forEach(div =>{
+        if (div.style.display ==="none"){
+            div.style.display = "inline-block"
+        }else{
+            div.style.display ="none"
+        }
+    })
+}
+
+const darkNotApproved = ()=>{
+    const allDivs = document.querySelectorAll<HTMLElement>("div.content_subjects");
+    allDivs.forEach(div => {
+        if (div.classList.contains("approved")) {
+            div.style.opacity = "1";
+        } else {
+            div.style.opacity = inputDarkNotApproved.checked ? "0.5" : "1";
+        }
+    })
+}
+
+inputButtonOff.addEventListener("change", ()=>{
+    buttonOff();
+})
+
+inputOnlyCoursed.addEventListener("change",()=>{
+    hideOnlyCoursed();
+})
+
+inputDarkNotApproved.addEventListener("change",()=>{
+    darkNotApproved();
+})
 
 loadCareer("sistemas","../data/ingenieria_sistemas.json");
